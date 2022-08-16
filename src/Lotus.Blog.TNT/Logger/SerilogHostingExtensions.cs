01beb3d61dc1f09a4b9dc324962c8b2b43b348cf
wangyou233 <915589928@qq.com>
2022-08-16 11:42:21 +0800

    Update SerilogHostingExtensions.cs

--- src/Lotus.Blog.TNT/Logger/SerilogHostingExtensions.cs
+++ src/Lotus.Blog.TNT/Logger/SerilogHostingExtensions.cs
@@ -13,6 +13,10 @@ namespace Lotus.Blog.TNT.Logger
 {
     public static class SerilogHostingExtensions
     {
+        /// <summary>
+        /// 加载Serilog配置文件
+        /// </summary>
+        /// <returns></returns>
         public static IConfiguration AddDefaultConfiguration()
         {
             var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
@@ -23,6 +27,11 @@ namespace Lotus.Blog.TNT.Logger
             .AddEnvironmentVariables()
             .Build();
         }
+        /// <summary>
+        /// 使用Serilog日志记录 以文件的方式记录
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <returns></returns>
         public static IWebHostBuilder UseSerilogDefault(this IWebHostBuilder hostBuilder)
         {
             
@@ -37,6 +46,12 @@ namespace Lotus.Blog.TNT.Logger
             return hostBuilder;
 
         }
+        /// <summary>
+        /// 获取log文件
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
         public static string GetLogFile(this IConfiguration config, string defaultValue)
         {
             return config.GetValue<string>("LogFile", defaultValue);
